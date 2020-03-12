fdfd3022ac9211bd93e137c8d32537c398ee62e5
Soba <15152226+sobaau@users.noreply.github.com>
2020-03-13 00:42:34 +1000

    Added missing images

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+console.log(screen.width)
+const resize = () => {
+  const workhis = document.getElementById('work-hist');
+  const skill = document.getElementById('edu-his');
+  skill.classList.remove('fl1');
+  workhis.classList.remove('fl1');
+
+}
+
+window.onload = () =>{
+  //resize();
+}
