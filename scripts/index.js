3c88c3813a24cfae962d4c263943a2552966c7f8
Soba <15152226+sobaau@users.noreply.github.com>
2020-03-13 16:02:52 +1000

    Mobile Design

--- scripts/index.js
+++ scripts/index.js
@@ -3,9 +3,9 @@ console.log(screen.width)
 const navSlide = () => {
   const hamburger = document.querySelector('.hamburger');
   const nav = document.querySelector('.navlinks');
-  const navLinks = document.querySelectorAll('.navlinks li')
+  const navLinks = document.querySelectorAll('.navlinks li');
   hamburger.addEventListener('click', () => {
-    nav.classList.toggle('nav-active')
+    nav.classList.toggle('nav-active');
     navLinks.forEach((link, index) => {
       if (link.style.animation) {
         link.style.animation = '';
@@ -13,9 +13,40 @@ const navSlide = () => {
         link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
       }
     })
-    hamburger.classList.toggle('toggle')
+    hamburger.classList.toggle('toggle');
   })
+
+  const wrapper = document.querySelector('.wrapper');
+  wrapper.addEventListener('click', () => {
+    if (nav.classList.contains('nav-active')) {
+      nav.classList.toggle('nav-active');
+    }
+  });
+}
+navSlide();
+const navResize = () => {
+  const nav = document.querySelector('.navlinks');
+  const hamburger = document.querySelector('.hamburger');
+  const navLinks = document.querySelectorAll('.navlinks li');
+
+  if (nav.classList.contains('nav-active')) {
+    nav.classList.toggle('nav-active');
+    hamburger.classList.toggle('toggle');
+    navLinks.forEach((link, index) => {
+      link.style.animation = '';
+    })
+  }
+  if(!nav.classList.contains('navslide')){
+    document.querySelector('.navlinks').classList.add('navslide')
+  }
 
 }
 
-navSlide();
+window.onresize = () => {
+  document.querySelector('.navlinks').classList.remove('navslide')
+  navResize();
+}
+
+window.onload = () =>{
+  document.querySelector('.navlinks').classList.add('navslide')
+}
