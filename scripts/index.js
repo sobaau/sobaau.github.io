9c2678347d7718799e200eabb53317ac3ba80005
Soba <15152226+sobaau@users.noreply.github.com>
2020-03-13 14:52:19 +1000

    Mobile Design

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,21 @@
 console.log(screen.width)
-const resize = () => {
-  const workhis = document.getElementById('work-hist');
-  const skill = document.getElementById('edu-his');
-  skill.classList.remove('fl1');
-  workhis.classList.remove('fl1');
+
+const navSlide = () => {
+  const hamburger = document.querySelector('.hamburger');
+  const nav = document.querySelector('.navlinks');
+  const navLinks = document.querySelectorAll('.navlinks li')
+  hamburger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active')
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = '';
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+      }
+    })
+    hamburger.classList.toggle('toggle')
+  })
 
 }
 
-window.onload = () =>{
-  //resize();
-}
+navSlide();
